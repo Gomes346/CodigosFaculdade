84f02c933f30fd8d87975de6ebd83757a58361d3
Gomes346 <gabrielggomes346@gmail.com>
2023-05-26 20:30:21 -0300

    Exercicio Conteudo8

---
+++ src/ExerciciosGeral/c08ex17.java
@@ -0,0 +1,11 @@
+package ExerciciosGeral;
+
+public class c08ex17 {
+    public static void main(String[] args) {
+        double fx;
+        for (int x=3;x<=12;x++){
+            fx=Math.sqrt(Math.pow(x,2)-3);
+            System.out.println("X= "+x+" F(x)= "+fx);
+        }
+    }
+}
