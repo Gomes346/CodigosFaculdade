513d2f619c99d4806c5593036905e3c957188ed0
Gomes346 <gabrielggomes346@gmail.com>
2023-05-30 19:46:41 -0300

    Exercicio Conteudo9

---
+++ src/ExerciciosGeral/c09ex01.java
@@ -0,0 +1,14 @@
+package ExerciciosGeral;
+
+public class c09ex01 {
+    public static void main(String[] args) {
+        double num, den, serie = 0;
+        for (int aux = 1; aux <= 37; aux++) {
+            num = (38 - aux) * (39 - aux);
+            den = aux;
+            serie += num / den;
+        }
+        System.out.print("Valor da Série = " + serie);
+    }
+}
+
